0f6d3e33ae4945240d9941bdf253cf305087bf4d
MuriloTomato <murilo.a.souza@outlook.com>
2021-04-07 16:53:00 -0300

    Atualização.

--- Form1.cs
+++ Form1.cs
@@ -29,17 +29,23 @@ namespace Calculadora_de_Bhaskara_e_Delta
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int A2, B2, C2;
-            double DELTA, BHASKARAPOSITIVO, BHASKARANEGATIVO;
-            A2 = int.Parse(A.Text);
-            B2 = int.Parse(B.Text);
-            C2 = int.Parse(C.Text);
+            double A2, B2, C2;
+            double DELTA, BHASKARAPOSITIVO, BHASKARANEGATIVO, RAIZ1, t1, t2, d1;
+            A2 = double.Parse(A.Text);
+            B2 = double.Parse(B.Text);
+            C2 = double.Parse(C.Text);
             DELTA = B2 * B2 - 4 * A2 * C2;
-            MessageBox.Show("O valor de Delta é: " + DELTA, "Resultado", MessageBoxButtons.OK);
-            BHASKARAPOSITIVO = -B2 + √DELTA / 2 * A2;
-            MessageBox.Show("O valor de Bhaskara Positiva é: " + BHASKARAPOSITIVO, "Resultado", MessageBoxButtons.OK);
-            BHASKARANEGATIVO = -B2 - √DELTA / 2 * A2;
-            MessageBox.Show("O valor de Bhaskara Negativa é: " + BHASKARANEGATIVO, "Resultado", MessageBoxButtons.OK);
+            RAIZ1 = Math.Sqrt(DELTA);
+            BHASKARAPOSITIVO = -B2 + RAIZ1;
+            d1 = 2 * A2;
+            t1 = BHASKARAPOSITIVO / d1;
+            BHASKARANEGATIVO = -B2 - RAIZ1;
+            t2 = BHASKARANEGATIVO / d1;
+            MessageBox.Show("Resultado de Delta é: " + DELTA, "Resultado", MessageBoxButtons.OK);
+            MessageBox.Show("Resultado de Bhaskara Positivo é: " + t1, "Resultado", MessageBoxButtons.OK);
+            MessageBox.Show("Resultado de Bhaskara Negativo é: " + t2, "Resultado", MessageBoxButtons.OK);
+            
+
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
